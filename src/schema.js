8080e74a341ec613b4e44f32b4644c8db1a245c7
NivedhaRamaraj <ramarajnivedhaa@gmail.com>
2022-09-28 15:21:40 +0530

    updated resolvers and schema

--- src/schema.js
+++ src/schema.js
@@ -35,9 +35,11 @@ const typeDefs = gql`
     author: Author!
     "The track's illustration to display in track card or track page detail"
     thumbnail: String
-    "The track's approximate length to complete, in minutes"
-    length: Int
+    "The track's approximate length to complete, in seconds"
+     length: Int @deprecated(reason: "Use durationInSeconds")
     "The number of modules this track contains"
+    durationInSeconds: Int
+    "The track's full duration, in seconds"
     modulesCount: Int
     "The track's complete description, can be in markdown format"
     description: String
@@ -61,9 +63,11 @@ const typeDefs = gql`
     id: ID!
     "The module's title"
     title: String!
-    "The module's length in minutes"
-    length: Int
+   "The track's approximate length to complete, in seconds"
+    length: Int @deprecated(reason: "Use durationInSeconds")
     "The module's text-based description, can be in markdown format. In case of a video, it will be the enriched transcript"
+    durationInSeconds: Int 
+    "The module's video duration, in seconds"
     content: String
     "The module's video url, for video-based modules"
     videoUrl: String
